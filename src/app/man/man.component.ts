ee629cabaa3167a3e9dc63967b544a5a24f5369c
ismoilsirojiddinov77-enggit config user.gmail ismoilsirojiddinov77@gmail.com <ismoilsirojiddinov77@gmail.com>
2025-08-27 08:43:39 +0500

    asdsa

--- src/app/man/man.component.ts
+++ src/app/man/man.component.ts
@@ -1,6 +1,8 @@
+
 import { Component, inject } from '@angular/core';
 import { AllserveService } from '../allserve.service';
 import { NgIf } from '@angular/common';
+
 @Component({
   selector: 'app-man',
   standalone: true,
@@ -9,17 +11,35 @@ import { NgIf } from '@angular/common';
   styleUrl: './man.component.css'
 })
 export class ManComponent {
-  server = inject(AllserveService)
+  server = inject(AllserveService);
 
-    isOpen: { [key: string]: boolean } = {
+  isOpen: { [key: string]: boolean } = {
     fit: false,
     features: false,
-    size:false
+    size: false
   };
 
   toggleFilter(section: string) {
     this.isOpen[section] = !this.isOpen[section];
   }
+
+  toggleColor(color: string) {
+    this.server.toggleFilter(this.server.selectedColors, color);
+  }
+
+  toggleBrand(brand: string) {
+    this.server.toggleFilter(this.server.selectedBrands, brand);
+  }
+
+  toggleSize(size: string) {
+    this.server.toggleFilter(this.server.selectedSizes, size);
+  }
+
+  toggleType(type: string) {
+    this.server.toggleFilter(this.server.selectedTypes, type);
+  }
+
+  clearTypes() {
+    this.server.selectedTypes.set(new Set());
+  }
 }
-
-
--- src/app/savat/savat.component.ts
+++ src/app/savat/savat.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AllserveService } from '../allserve.service';
 
 @Component({
   selector: 'app-savat',
@@ -8,5 +9,56 @@ import { Component } from '@angular/core';
   styleUrl: './savat.component.css'
 })
 export class SavatComponent {
+  server = inject(AllserveService);
 
+  // Jami summani hisoblash
+  getTotalPrice(): number {
+    return this.server.savatWithUpdatedStatus().reduce((total, item) => {
+      return total + (item.price * (item.num || 1));
+    }, 0);
+  }
+
+  // Mahsulot sonini oshirish
+  increaseQuantity(itemId: number) {
+    const currentSavat = this.server.savat();
+    const updatedSavat = currentSavat.map(item => {
+      if (item.id === itemId) {
+        return { ...item, num: (item.num || 1) + 1 };
+      }
+      return item;
+    });
+    this.server.savat.set(updatedSavat);
+  }
+
+  // Mahsulot sonini kamaytirish
+  decreaseQuantity(itemId: number) {
+    const currentSavat = this.server.savat();
+    const item = currentSavat.find(item => item.id === itemId);
+    
+    if (item && (item.num || 1) <= 1) {
+      // Agar miqdor 1 ga teng yoki kichik bo'lsa, savatdan olib tashlash
+      this.server.removeFromSavat(itemId);
+    } else {
+      // Miqdorni kamaytirish
+      const updatedSavat = currentSavat.map(item => {
+        if (item.id === itemId) {
+          return { ...item, num: (item.num || 1) - 1 };
+        }
+        return item;
+      });
+      this.server.savat.set(updatedSavat);
+    }
+  }
+
+  // Savatni tozalash
+  clearCart() {
+    // Barcha itemlarning basket holatini false qilish
+    const savatItems = this.server.savat();
+    savatItems.forEach(item => {
+      this.server.basket(item.id);
+    });
+    
+    // Savatni tozalash
+    this.server.savat.set([]);
+  }
 }
